ccfd249e7c6c362dcca597e22db7540f39b8ece4
Madhuri Goud <madhurigoud02@gmail.com>
2025-12-23 21:31:03 -0600

    Initial clean commit - Medical Chatbot using RAG

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+
+setup(
+    name="Medical_Chatbot",
+    version="0.1.0",
+    author="Jagannathan",
+    author_email="Madhurigoud02@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
